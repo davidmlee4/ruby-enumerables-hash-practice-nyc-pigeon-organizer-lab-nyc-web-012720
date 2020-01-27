a7dfe77c5b7f6b6849140ca5d91d27cca62ec150
Muhan Lee <davidmlee4@gmail.com>
2020-01-27 01:30:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,14 @@ def nyc_pigeon_organizer(data)
   result = {}
   data.each {|key,value|
     value.each{|new_value,names|
-      names.each{|n| }
+      names.each{|n| 
+
+        if !result[name]
+          result[name] = {}
+        end
+
+        if !result[name][key]
+          result[name][key] = []
 
 
 
