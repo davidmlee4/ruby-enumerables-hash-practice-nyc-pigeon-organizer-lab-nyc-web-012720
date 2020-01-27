12a3a6739bb7c6dd5b20706c392b18b49b975c70
Muhan Lee <davidmlee4@gmail.com>
2020-01-27 01:35:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,22 +1,23 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   result = {}
+
   data.each {|key,value|
-    value.each{|new_value,names|
-      names.each{|n|
+    value.each {|new_value,names|
+      names.each {|name|
+        
+    if !result[name]
+      result[name] = {}
+    end
 
-        if !result[name]
-          result[name] = {}
-        end
+    if !result[name][key]
+      result[name][key] = []
+    end
 
-        if !result[name][key]
-          result[name][key] = []
-        end
-
-        result[name][key] << new_value.to_s
+    result[name][key] << new_value.to_s
 
 
-      }}}
-      result
+    }}}
+    result
 
 end
